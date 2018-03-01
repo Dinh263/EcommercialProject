1620dcd98ad5114d489a98b1d6cf140305318aa4
Dinh263 <dinh.test263@hotmail.com>
2018-03-01 08:15:27 -0800

    add asserttion library

--- com.tutorial.Automation.Ecomercesite/src/test/java/Alert/AlertHelper.java
+++ com.tutorial.Automation.Ecomercesite/src/test/java/Alert/AlertHelper.java
@@ -1,5 +1,38 @@
 package Alert;
 
-public class AlertHelper {
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
 
+public class AlertHelper {
+	private WebDriver driver;
+	public AlertHelper(WebDriver driver) {
+		this.driver=driver;
+	}
+	
+	public Alert getAlert() {
+		return driver.switchTo().alert();
+	}
+	
+	public void acceptAlert() {
+		getAlert().accept();
+	}
+	
+	public void dismissAlert() {
+		getAlert().dismiss();
+	}
+	
+	public String getAlertText() {
+		String text =getAlert().getText();
+		return text;
+	}
+	
+	public boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		}catch(NoAlertPresentException ex) {
+			return false;
+		}
+	}
 }
